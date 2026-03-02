dc4996c5fd9ee079d67c104e078d3aeec2135230
Mario Winkler <mario.winkler@dlr.de>
2026-03-02 11:27:49 +0100

    EOPF theming

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     catalogUrl: null,
     catalogTitle: "STAC Browser",
-    catalogImage: null,
+    catalogImage: "https://raw.githubusercontent.com/EOPF-Sample-Service/eopf-stac/main/thumbnails/EOPF-on-dark.png",
     allowExternalAccess: true, // Must be true if catalogUrl is not given
     allowedDomains: [],
     detectLocaleFromBrowser: true,
